46497b769065710d91b948f4271ec581f77b9bf6
John Murowaniecki <jmurowaniecki@gmail.com>
2020-05-10 09:43:22 -0300

    Dragons flying, showing some info, preparing to create forms

--- app/src/app/screen/pannel/stage/stage.component.ts
+++ app/src/app/screen/pannel/stage/stage.component.ts
@@ -10,6 +10,14 @@ export class StageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    let dde = document.documentElement;
+    dde.addEventListener("mousemove", e => {
+      let ow = dde.offsetWidth;
+      let oh = dde.offsetHeight;
+      dde.style.setProperty('--mouseX', String(e.clientX * 100 / ow));
+      dde.style.setProperty('--mouseY', String(e.clientY * 100 / oh));
+    });
   }
 
 }
