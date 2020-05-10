260a1db3b6ea105f74775d4432bd86fb97ff96c5
John Murowaniecki <jmurowaniecki@gmail.com>
2020-05-10 05:33:58 -0300

    Adding services to handle certain activities and starting some screens

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Librum ex Dracones';
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      document.body.classList.toggle('show');
+      setTimeout(() => document.body.classList.toggle('ready'), 1000);
+    }, 3000);
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './screen/login/login.component';
+import { StatusService } from './service/status.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [StatusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
